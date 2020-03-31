02a837f5fbe4af2ced5471eb9eb234fb0480f69c
Joan Santos <joansants07@gmail.com>
2020-03-30 23:55:10 -0300

    Config router

--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 import './index.css';
 import App from './App';
 
+const app = (
+	<BrowserRouter>
+		<Switch>
+			<Route path='/' exact={true} component={App} />
+		</Switch>
+	</BrowserRouter>
+);
+
 ReactDOM.render(
-	<React.StrictMode>
-		<App />
-	</React.StrictMode>,
+	<React.StrictMode>{app}</React.StrictMode>,
 	document.getElementById('root')
 );
