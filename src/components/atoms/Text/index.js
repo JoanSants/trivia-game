9c48297ac75e9be0e1301b03df0ac0a2e83a55a3
Joan Santos <joansants07@gmail.com>
2020-04-02 21:58:11 -0300

    Change sass files to css

--- src/components/atoms/Text/index.js
+++ src/components/atoms/Text/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-import './index.sass';
+import './index.css';
 
 const Text = ({ children, size }) => (
 	<p className={`Text Text--${size}`} data-test="Text">
