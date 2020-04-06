a93fdbf1ca2d4fabae6d343f15ad07616b2f8d74
Joan Santos <joansants07@gmail.com>
2020-04-06 20:58:24 -0300

    Add HomeTemplate component

--- src/components/templates/Home/index.js
+++ src/components/templates/Home/index.js
@@ -1,5 +1,24 @@
 import React from 'react';
 
-const HomeTemplate = () => <div data-test="HomeTemplate" />
+import './index.css';
+import Text from '../../atoms/Text';
+import Button from '../../atoms/Button';
+import ImgSvg from '../../atoms/ImgSvg';
+
+const HomeTemplate = () => (
+	<div data-test="HomeTemplate" className="HomeTemplate">
+		<Text>Welcome to the Trivia Challenge!</Text>
+		<div className="HomeTemplate__description">
+			<div className="HomeTemplate__description__logo">
+				<ImgSvg name="clipboard" />
+			</div>
+			<Text size="small">You will be presented with 10 True or False questions.</Text>
+		</div>
+		<Text>Can you score 100%?</Text>
+		<Button fullWidth onClick={() => {}}>
+			Begin
+		</Button>
+	</div>
+);
 
 export default HomeTemplate;
