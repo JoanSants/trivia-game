441891a02ea0fcb433f871dc9267941123ea6a7a
Joan Santos <joansants07@gmail.com>
2020-04-08 20:14:02 -0300

    Add result route

--- src/App.js
+++ src/App.js
@@ -4,11 +4,13 @@ import { Switch, Route } from 'react-router-dom';
 import './App.css';
 import HomePage from './components/pages/Home';
 import QuizPage from './components/pages/Quiz';
+import ResultPage from './components/pages/Result';
 
 const App = () => (
 	<div className='App' data-test='App'>
 		<Switch>
 			<Route path="/quiz" component={QuizPage} />
+			<Route path="/result" component={ResultPage} />
 			<Route path="/" exact component={HomePage} />
 		</Switch>
 	</div>
