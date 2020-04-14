916eea07c5e5c5f4a2f46bcfa9e2d28b579b166c
Joan Santos <joansants07@gmail.com>
2020-04-14 20:29:03 -0300

    Add option to disable button

--- src/components/atoms/Button/index.js
+++ src/components/atoms/Button/index.js
@@ -3,23 +3,36 @@ import PropTypes from 'prop-types';
 
 import './index.css';
 
-const Button = ({ children, onClick, fullWidth }) => (
-	<button 
-		className={`Button ${fullWidth ? 'Button--full' : ''}`}
-		onClick={onClick}
-		data-test="Button"
-	>
-		{children}
-	</button>
-);
+const Button = ({
+	children,
+	onClick,
+	fullWidth,
+	disabled
+}) => {
+	const classes = ['Button'];
+	if (fullWidth) classes.push('Button--full');
+	if (disabled) classes.push('Button--disabled');
+
+	return (
+		<button
+			className={classes.join(' ')}
+			onClick={disabled ? () => {} : onClick}
+			data-test="Button"
+		>
+			{children}
+		</button>
+	);
+};
 
 Button.propTypes = {
 	onClick: PropTypes.func.isRequired,
-	fullWidth: PropTypes.bool
+	fullWidth: PropTypes.bool,
+	disabled: PropTypes.bool
 };
 
 Button.defaultProps = {
-	fullWidth: false
+	fullWidth: false,
+	disabled: false
 };
 
 export default Button;
